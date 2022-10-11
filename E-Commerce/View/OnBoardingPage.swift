052ae7950d28aa12c9bf40b0d4048156bdc3dfb6
QuangHa <duongthien.mcs@gmail.com>
2022-10-11 17:39:53 +0700

    1110

---
+++ E-Commerce/View/OnBoardingPage.swift
@@ -0,0 +1,68 @@
+//
+//  OnBoardingPage.swift
+//  E-Commerce
+//
+//  Created by Nguyen Quang Ha on 11/10/2022.
+//
+
+import SwiftUI
+
+let customFont = "Raleway-Regular"
+
+struct OnBoardingPage: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Find your\nGadget")
+                .font(.custom(customFont, size: 55))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            
+            Image("OnBoardingImage_2")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Button {
+                
+            } label: {
+                Text("Get Started")
+                    .font(.custom(customFont, size: 18))
+                    .fontWeight(.semibold)
+                    .padding(.vertical, 18)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.1),
+                            radius: 5, x: 5, y: 5)
+                    .foregroundColor(Color("Main"))
+            }
+            .padding(.horizontal, 30)
+            // Add dome adjusments only for larger displays
+            .offset(y: getRect().height < 750 ? 20 : 40)
+            
+            Spacer()
+        }
+        .padding()
+        .padding(.top, getRect().height < 750 ? 0 : 20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            Color("Main")
+        )
+    }
+}
+
+struct OnBoardingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingPage()
+            .previewDevice("iPhone 12 Pro Max")
+        
+        OnBoardingPage()
+            .previewDevice("iPhone 8")
+    }
+}
+
+// extending view
+extension View {
+    func getRect() -> CGRect {
+        return UIScreen.main.bounds
+    }
+}
---
+++ E-Commerce/ViewModel/LoginPageModel.swift
@@ -0,0 +1,20 @@
+//
+//  LoginPageModel.swift
+//  E-Commerce
+//
+//  Created by Nguyen Quang Ha on 11/10/2022.
+//
+
+import SwiftUI
+
+struct LoginPageModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginPageModel_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPageModel()
+    }
+}
